ec2d2cf1b65b2a528f98887cca08e9586485fa4b
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2021-11-15 17:37:05 +0200

    association

--- .history/db/migrate/20211115145305_create_listings_20211115165304.rb
+++ .history/db/migrate/20211115145305_create_listings_20211115165304.rb
@@ -6,7 +6,7 @@ class CreateListings < ActiveRecord::Migration[6.0]
       t.integer :price
       t.string :location
       t.date :availability
-      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
