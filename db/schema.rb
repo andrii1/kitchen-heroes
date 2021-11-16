355a9c263a8b0a93e3b3bd4f984c1626ded1cd69
Marie-Luise Hahn <marieluisehahn@ymail.com>
2021-11-16 18:01:28 +0100

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -51,10 +51,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_151549) do
     t.integer "price"
     t.string "location"
     t.date "availability"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,5 +73,4 @@ ActiveRecord::Schema.define(version: 2021_11_16_151549) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "listings"
   add_foreign_key "bookings", "users"
-  add_foreign_key "listings", "users"
 end
