2af5cef587f99917c0981d9d204e76510c2d4b77
Paul Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2021-11-16 15:58:37 +0100

    buttons and grid for index

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 2021_11_15_162136) do
     t.integer "price"
     t.string "location"
     t.date "availability"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_11_15_162136) do
 
   add_foreign_key "bookings", "listings"
   add_foreign_key "bookings", "users"
+  add_foreign_key "listings", "users"
 end
