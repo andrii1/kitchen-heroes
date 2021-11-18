26d491595982d92f87af4a435a8cd847d888b817
Paul Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2021-11-18 12:32:27 +0100

    bookings backend

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,23 @@
+class BookingsController < ApplicationController
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @listing = Listing.find(params[:listing_id])
+    @booking.listing = @listing
+    if @booking.save
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date_booking)
+  end
+end
